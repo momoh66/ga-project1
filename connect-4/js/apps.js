a9393d50a6c50049db0737bdfc68429f6347e4d0
Mohamed Mohamed <m.h.mohamed@hotmail.co.uk>
2022-02-24 14:59:37 +0000

    almost done !

--- connect-4/js/apps.js
+++ connect-4/js/apps.js
@@ -1,71 +1,253 @@
-// the grid
+//! Variables
 const grid = document.querySelector('.grid');
-console.log(grid);
-
-//event listener for the player to pick a positon on the grid
-
+const result = document.querySelector('.results');
+const reset = document.querySelector('button.reset');
+const selectionButton = document.querySelectorAll('div .button button');
+const twoPlayer = document.querySelector('.2-player');
 const column = 7;
 const row = 6;
 const gridCellCount = row * column;
 const cells = [];
 let player = 1;
 
-function createGrid() {
-  for (let i = 0; i < gridCellCount; i++) {
-    const cell = document.createElement('div');
-    cell.setAttribute('data-id', i);
-    cell.innerHTML = i;
-    // cell.classList.add('red-counter');
-    cells.push(cell);
-    grid.appendChild(cell);
+//! mouse in
+for (let i = 0; i <= 6; i++) {
+  selectionButton[i].addEventListener('mouseover', (event) =>
+    selectionButtonHoverIn(event, i)
+  );
+}
+function selectionButtonHoverIn(event, buttonId) {
+  buttonId;
+  // console.log(buttonId);
+  if (player === 1) {
+    selectionButton[buttonId].style.backgroundColor = 'red';
+  } else {
+    selectionButton[buttonId].style.backgroundColor = 'blue';
   }
 }
 
+//! mouse out
+for (let i = 0; i <= 6; i++) {
+  selectionButton[i].addEventListener('mouseout', (event) =>
+    selectionButtonHoverOut(event, i)
+  );
+}
+function selectionButtonHoverOut(event, buttonId) {
+  buttonId;
+  console.log(buttonId);
+  if (player === 1) {
+    selectionButton[buttonId].style.backgroundColor = 'transparent';
+  } else {
+    selectionButton[buttonId].style.backgroundColor = 'transparent';
+  }
+}
+//! Creeate Grid
+function createGrid() {
+  let i = 0;
+  setInterval(() => {
+    if (i < gridCellCount) {
+      const cell = document.createElement('div');
+      grid.appendChild(cell);
+      cells.push(cell);
+    }
+    i++;
+  }, 25);
+}
 createGrid();
 
-console.log(cells);
-//event listener for all slots - so that onlcick the counter's colur is used
-// cells.forEach((a) => a.addEventListener('click', addCounter));
-
-for (let i = 35; i <= 41; i++) {
-  cells[i].addEventListener('click', addCounter);
+//!! Game logic for 2 player
+for (let i = 0; i <= 6; i++) {
+  selectionButton[i].disabled = false;
+  selectionButton[i].addEventListener('click', addCounter);
+  selectionButton[i].setAttribute('data-id', i + 35);
 }
 
 function addCounter(event) {
   const highestIndex = event.target.dataset.id;
+  console.log(highestIndex);
 
+  for (let i = 0; i <= 6; i++) {
+    selectionButton[i].style.backgroundColor = 'transparent';
+  }
   if (!cells[highestIndex].classList.contains('occupied')) {
     console.log(highestIndex, 'not occupied');
     if (player === 1) {
+      selectionButton[3].style.backgroundColor = 'blue';
       cells[highestIndex].classList.add('occupied', 'red-counter');
+      result.innerHTML = "Blue player's turn";
       player = 2;
     } else {
+      selectionButton[3].style.backgroundColor = 'red';
       cells[highestIndex].classList.add('occupied', 'blue-counter');
+      result.innerHTML = "Red player's turn";
       player = 1;
     }
   } else {
-    console.log(highestIndex, 'occupied');
     const columnArray = [];
-    // turn columnArray into an array of cell numbers that make up a column
+
     for (let i = highestIndex; i > 0; i = i - 7) {
       if (i - 7 >= 0) {
         columnArray.push(i - 7);
       }
     }
-
-    console.log(columnArray, highestIndex);
-
-    // return the number of the first cell that is not occupied
     const firstAvailable = columnArray.find((number) => {
       return !cells[number].classList.contains('occupied');
     });
 
     if (player === 1) {
       cells[firstAvailable].classList.add('occupied', 'red-counter');
+      selectionButton[3].style.backgroundColor = 'blue';
+      result.innerHTML = "Blue player's turn";
       player = 2;
     } else {
       cells[firstAvailable].classList.add('occupied', 'blue-counter');
+      selectionButton[3].style.backgroundColor = 'red';
+      result.innerHTML = "Red player's turn";
       player = 1;
     }
   }
+  //! Results
+
+  //? Horizontal wins
+  for (let i = 0; i < 6; i++) {
+    for (let j = 0; j < 4; j++) {
+      let k = 7 * i + j;
+
+      const winningHorizontal = [k, k + 1, k + 2, k + 3];
+      if (
+        cells[winningHorizontal[0]].classList.contains('red-counter') &&
+        cells[winningHorizontal[1]].classList.contains('red-counter') &&
+        cells[winningHorizontal[2]].classList.contains('red-counter') &&
+        cells[winningHorizontal[3]].classList.contains('red-counter')
+      ) {
+        result.innerHTML = 'Red Wins!!!';
+        for (let i = 0; i < 7; i++) {
+          selectionButton[i].disabled = true;
+        }
+      } else if (
+        cells[winningHorizontal[0]].classList.contains('blue-counter') &&
+        cells[winningHorizontal[1]].classList.contains('blue-counter') &&
+        cells[winningHorizontal[2]].classList.contains('blue-counter') &&
+        cells[winningHorizontal[3]].classList.contains('blue-counter')
+      ) {
+        result.innerHTML = 'Blue Wins!!!';
+        for (let i = 0; i < 7; i++) {
+          selectionButton[i].disabled = true;
+        }
+      }
+    }
+  }
+  //? Vertical wins
+  for (let i = 0; i < 3; i++) {
+    for (let j = 0; j < 7; j++) {
+      let k = 7 * i + j;
+      const winningVertical = [k, k + 7, k + 14, k + 21];
+      if (
+        cells[winningVertical[0]].classList.contains('red-counter') &&
+        cells[winningVertical[1]].classList.contains('red-counter') &&
+        cells[winningVertical[2]].classList.contains('red-counter') &&
+        cells[winningVertical[3]].classList.contains('red-counter')
+      ) {
+        result.innerHTML = 'Red Wins!!!';
+        for (let i = 0; i < 7; i++) {
+          selectionButton[i].disabled = true;
+        }
+      }
+      if (
+        cells[winningVertical[0]].classList.contains('blue-counter') &&
+        cells[winningVertical[1]].classList.contains('blue-counter') &&
+        cells[winningVertical[2]].classList.contains('blue-counter') &&
+        cells[winningVertical[3]].classList.contains('blue-counter')
+      ) {
+        result.innerHTML = 'Blue Wins!!!';
+        for (let i = 0; i < 7; i++) {
+          selectionButton[i].disabled = true;
+        }
+      }
+    }
+  }
+  //? Decline slope wins
+  for (let i = 0; i < 3; i++) {
+    for (let j = 0; j < 4; j++) {
+      let k = 7 * i + j;
+      const winningDecline = [k, k + 8, k + 16, k + 24];
+      if (
+        cells[winningDecline[0]].classList.contains('red-counter') &&
+        cells[winningDecline[1]].classList.contains('red-counter') &&
+        cells[winningDecline[2]].classList.contains('red-counter') &&
+        cells[winningDecline[3]].classList.contains('red-counter')
+      ) {
+        result.innerHTML = 'Red Wins!!!';
+        for (let i = 0; i < 7; i++) {
+          selectionButton[i].disabled = true;
+        }
+      }
+      if (
+        cells[winningDecline[0]].classList.contains('blue-counter') &&
+        cells[winningDecline[1]].classList.contains('blue-counter') &&
+        cells[winningDecline[2]].classList.contains('blue-counter') &&
+        cells[winningDecline[3]].classList.contains('blue-counter')
+      ) {
+        result.innerHTML = 'Blue Wins!!!';
+        for (let i = 0; i < 7; i++) {
+          selectionButton[i].disabled = true;
+        }
+      }
+    }
+  }
+  //? Incline slope wins
+  for (let i = 0; i < 3; i++) {
+    for (let j = 0; j < 4; j++) {
+      let k = 7 * i + j + 3;
+      const winningIncline = [k, k + 6, k + 12, k + 18];
+
+      if (
+        cells[winningIncline[0]].classList.contains('red-counter') &&
+        cells[winningIncline[1]].classList.contains('red-counter') &&
+        cells[winningIncline[2]].classList.contains('red-counter') &&
+        cells[winningIncline[3]].classList.contains('red-counter')
+      ) {
+        result.innerHTML = 'Red Wins!!!';
+        for (let i = 0; i < 7; i++) {
+          selectionButton[i].disabled = true;
+        }
+      }
+      if (
+        cells[winningIncline[0]].classList.contains('blue-counter') &&
+        cells[winningIncline[1]].classList.contains('blue-counter') &&
+        cells[winningIncline[2]].classList.contains('blue-counter') &&
+        cells[winningIncline[3]].classList.contains('blue-counter')
+      ) {
+        result.innerHTML = 'Blue Wins!!!';
+        for (let i = 0; i < 7; i++) {
+          selectionButton[i].disabled = true;
+        }
+      }
+    }
+  }
+}
+
+for (let i = 0; i < 42; i++) {
+  const slots = cells[i];
+}
+functi;
+//! Reset button
+
+reset.addEventListener('click', resetEverything);
+
+function resetEverything() {
+  for (let i = 0; i < 42; i++) {
+    cells[i].classList.remove('red-counter', 'blue-counter', 'occupied');
+  }
+  result.innerHTML = `READY TO START, PLACE MOUSE OVER THE TOP OF THE BOARD TO PICK A SLOT`;
+  for (let i = 0; i < 7; i++) {
+    selectionButton[i].disabled = false;
+  }
+}
+//! Two - player
+
+twoPlayer.addEventListener('click', twoPlayerMode);
+
+function twoPlayerMode() {
+  addCounter();
 }
---
+++ connect-4/readme.md
@@ -0,0 +1,47 @@
+# Connect 4
+
+#### By Mohamed Mohamed
+
+## Overview
+
+This is Project 1 of my GA software engineering immersive course. The goals are to create a fully functioning game with logic in order to output a winner. This ought to be illustrated with key skills from HTML, CSS and JS.
+
+## The rules
+
+Connect 4 is a game where players attempt to make a line of four pieces in a 7 x 6 grid. Players can drop their pieces into columns, so that their piece rests in the lowest available space in that column.
+
+The winner is the first to create a line of four in any direction, including diagonally. If the board is filled before a line of 4 can be made, the game is declared a draw.
+
+## HTML
+
+I sculpted my whole project around my HTMl. After having planned what elements I'd need in my HTML file, I decided what skills I could use in the CSS and JS in order to have a fully rounded project. I kept my HTML document very simple in order that I showcase my ability to use JS along with CSS to manipulate a very simple HTML document.
+
+## CSS
+
+I used flexbox avidly in this project. Again my previous experience with the tutorials really came into fruition here. There were some challenges in order to have certain aspects the way I wanted but I'd say the learning curve though steep was very much appreciated. I feel my ability to use CSS to aesthetically manipulate any project from here on in will grow from strength to strength. I thoroughly enjoyed using resources like mdn and css tricks to diagnose and resolve problems.
+
+## JS
+
+Challenging is an understatement. The planning for this a part was an integral part of having a fully functioning game comissioned properly.
+I broke this part into several different chunks in order to simplify - what I considered to be a huge task.
+Firstly I developed a way to create a playing arena, in this instance a grid.
+
+I had to think throughly about how dynamic attribution of class would be integral to the game logic.
+
+I thereafter proceeded to creating a bomb proof logic which allow the counters of different colours to placed by the user.
+
+After having done this I proceeded to initiate comprehensive logic in order for the game to dictate a winner.
+On completion of those tasks I went on to you use query selectors to dynamically change the HTML. To match the logic in JS.
+
+## What I learned.
+
+Although this project was very challenging. I have consolidated knowledge of many aspects of HTML, CSS and JS including things like :
+
+- For loops along with nested for loops
+- understanding data types which is integral in order to manipulate code.
+- use of the inspect tool including the console
+- using querySelectors to target different parts of the program.
+- use of flexbox
+- use of wide range of element types in HTMl
+
+Given the length of the project I also picked key skills in time management along with the importance of using psedocode to code thoroughly.
